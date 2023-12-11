9b84479946cf51951f1bb9f5880632a0dca253b2
robin marillia <robin4a4@gmail.com>
2023-12-11 23:41:34 +0100

    sleep

--- packages/example/views/index.tsx
+++ packages/example/views/index.tsx
@@ -1,7 +1,8 @@
 import Counter from "./Counter";
 
 export default async function Home() {
-
+    const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+    await sleep(1000);
     const data = await fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json());
     return <>
         <h1>Home yes tu ne reves pas</h1>
