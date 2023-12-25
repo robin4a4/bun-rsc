16e36226b5e544e573395c2e819b820a9393a81a
robin marillia <robin4a4@gmail.com>
2023-12-25 20:29:21 +0100

    try mapping

--- packages/example/views/index.tsx
+++ packages/example/views/index.tsx
@@ -1,4 +1,4 @@
-// import Counter from "../components/Counter";
+import Counter from "../components/Counter";
 
 export default async function Home() {
     const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
@@ -6,7 +6,7 @@ export default async function Home() {
     const data = await fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json());
     return <>
         <h1>Home yes tu ne reves pas</h1>
-        {/* <Counter /> */}
+        <Counter />
         {data.map((todo: any) => {
             return <p>{todo.title}</p>
         })}
