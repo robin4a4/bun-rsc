d36f0c4f893fb08ea25cf1248bb479c46a5b8e84
robinmarillia <robin.m@hellowatt.fr>
2024-03-06 13:28:12 +0100

    fix readme and  data fetching

--- packages/example/basic-tailwind/src/pages/index.tsx
+++ packages/example/basic-tailwind/src/pages/index.tsx
@@ -17,7 +17,7 @@ export async function Page() {
 		currentTodos = await currentTodosFile.text();
 	}
 	return (
-		<main className="pt-8 bg-slate-100">
+		<main className="pt-8 bg-slate-100 h-screen">
 			<section className="container mx-auto flex flex-col gap-4">
 				<h1 className="text-xl font-bold">Hello, world!</h1>
 				<section className="bg-white rounded-lg shadow-lg p-4 flex flex-col gap-4">
@@ -46,11 +46,8 @@ async function Data() {
 	// fake latency
 	await new Promise((resolve) => setTimeout(resolve, 1000));
 
-	// const data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
-	// 	(res) => res.json(),
-	// );
-	const data = {
-		title: "Data",
-	};
+	const data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
+		(res) => res.json(),
+	);
 	return <p>{data.title}</p>;
 }
