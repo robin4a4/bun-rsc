e49d87dcc6f238459ae7fdb99eff7c0821144541
robinmarillia <robin.m@hellowatt.fr>
2024-03-13 13:18:16 +0100

    fix dev mode

--- packages/example/basic-tailwind/src/components/Nav.tsx
+++ packages/example/basic-tailwind/src/components/Nav.tsx
@@ -1,14 +1,14 @@
 export function Nav({currentRoute}: {currentRoute: string}) {
     return (
-            <ul className="bg-white shadow-lg rounded-lg flex gap-16 p-8">
-                <li>
-                    <a className={`px-16 py-8 rounded-lg ${currentRoute === "home" ? "bg-gray-100" : null}`} href="/">Home</a>
+            <ul className="bg-white shadow-lg rounded-lg flex gap-4 p-2 mb-16 container mx-auto">
+                <li className={`px-8 py-4 rounded-lg ${currentRoute === "home" ? "bg-gray-100" : null}`}>
+                    <a href="/">Home</a>
                 </li>
-                <li>
-                    <a className={`px-16 py-8 rounded-lg ${currentRoute === "form" ? "bg-gray-100" : null}`} href="/form">Form</a>
+                <li className={`px-8 py-4 rounded-lg ${currentRoute === "form" ? "bg-gray-100" : null}`}>
+                    <a href="/form">Form</a>
                 </li>
-                <li>
-                    <a className={`px-16 py-8 rounded-lg ${currentRoute === "data" ? "bg-gray-100" : null}`} href="/data">Data</a>
+                <li className={`px-8 py-4 rounded-lg ${currentRoute === "data" ? "bg-gray-100" : null}`}>
+                    <a href="/data">Data</a>
                 </li>
             </ul>
     );
