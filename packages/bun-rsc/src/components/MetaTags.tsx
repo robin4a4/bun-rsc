630eca741c99a97868e4084514839a0f7ab9117b
robin marillia <robin4a4@gmail.com>
2024-04-04 19:05:44 +0200

    viewport

--- packages/bun-rsc/src/components/MetaTags.tsx
+++ packages/bun-rsc/src/components/MetaTags.tsx
@@ -1,10 +1,11 @@
-import type { Meta } from "../types";
+import type { Meta } from "../types/external";
 
 export function MetaTags({ meta }: { meta: Meta }) {
 	return (
 		<>
 			<title>{meta?.title ?? "My app"}</title>
 			<meta name="description" content={meta?.description} />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			{meta?.icon ? <link rel="icon" href={`./public/${meta?.icon}`} /> : null}
 		</>
 	);
