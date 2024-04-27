3410e19fe3145218611d5dad51c34dfa64110a4d
robin marillia <robin4a4@gmail.com>
2024-04-27 21:43:25 +0200

    fix BASE_RSC_SERVER_URL

--- packages/bun-rsc/src/utils/common.ts
+++ packages/bun-rsc/src/utils/common.ts
@@ -3,23 +3,23 @@ export const BUN_RSC_SPECIFIC_KEYWORD_STATICS = "__bun_rsc";
 export const RSC_CONTENT_TYPE = "text/x-component";
 
 export function combineUrl(path1: string, path2: string) {
-	let firstPath = path1;
-	let secondPath = path2;
-	if (path1.endsWith("/")) {
-		firstPath = path1.substring(0, path1.length - 1);
-	}
-	if (path2.startsWith("/")) {
-		secondPath = path2.substring(1);
-	}
-	return `${firstPath}/${secondPath}`;
+  let firstPath = path1;
+  let secondPath = path2;
+  if (path1.endsWith("/")) {
+    firstPath = path1.substring(0, path1.length - 1);
+  }
+  if (path2.startsWith("/")) {
+    secondPath = path2.substring(1);
+  }
+  return `${firstPath}/${secondPath}`;
 }
 
 export const BASE_RSC_SERVER_URL =
-	process.env.MODE === "development"
-		? "http://localhost:3001"
-		: `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` ?? "";
+  process.env.MODE === "development"
+    ? "http://localhost:3001"
+    : `http://${process.env.BASE_SERVER_PATH}:3001` ?? "";
 
 export function getCacheKey(url: string) {
-	const urlObj = new URL(url.replace(`/${BUN_RSC_SPECIFIC_KEYWORD}`, ""));
-	return urlObj.search ? urlObj.pathname + urlObj.search : urlObj.pathname;
+  const urlObj = new URL(url.replace(`/${BUN_RSC_SPECIFIC_KEYWORD}`, ""));
+  return urlObj.search ? urlObj.pathname + urlObj.search : urlObj.pathname;
 }
